57e15d5bbbc4205411c36ba9ed62cb452096928f
S331471193 <S331471193@LDHS-BCT-100921.ad.ocdsb.ca>
2019-11-28 09:49:17 -0500

    Create Project

---
+++ GETOUT/src/getout/GETOUT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getout;
+
+/**
+ *
+ * @author S331471193
+ */
+public class GETOUT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // fred smells
+    }
+    
+}
